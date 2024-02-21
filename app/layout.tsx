3d13ba24e3b543bce827f38dd4d53326cb773589
Emerson <emersonsmatthias@gmail.com>
2024-02-21 16:46:17 -0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { poppins } from "@/settings/fonts";
 import Header from "@/components/Header";
 
 export const metadata: Metadata = {
-  title: "Athena - Softwares & Marketing Digital",
+  title: "Athena - Sites & Marketing Digital",
   description: "Athena | Sites ● Sistemas ● Marketing Digital Oferecemos uma combinação perfeita de criação de sites e estratégias de marketing digital. Unimos o melhor dos dois mundos, proporcionando sites altamente performáticos e estratégias de marketing digital eficazes. Não só montamos o seu site de forma otimizada, mas também configuramos o seu Google Meu Negócio para máxima visibilidade online. Além disso, gerenciamos o tráfego pago da sua loja, proporcionando uma abordagem abrangente. Do design à entrega, garantimos um serviço personalizado que reflete a identidade única da sua marca. Simplifique sua presença digital conosco e destaque-se da concorrência. Contate-nos hoje mesmo para impulsionar seu negócio para o próximo nível!",
 };
 
